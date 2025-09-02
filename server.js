85272eee0cc77d2141bea39d5c356f2c6169f0e9
Reverse666S <carlbot0815@gmail.com>
2025-09-02 09:11:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.use(express.json());
 
 let links = [];
 let members = [];
+let users = [];
+let supports = [];
+let home = [];
 
 // API-Routen
 app.get("/api/links", (req, res) => res.json(links));
@@ -18,6 +21,24 @@ app.post("/api/links", (req, res) => {
 app.get("/api/members", (req, res) => res.json(members));
 app.post("/api/members", (req, res) => {
   members.push(req.body);
+  res.json({ success: true });
+});
+
+app.get("/api/users", (req, res) => res.json(users));
+app.post("/api/users", (req, res) => {
+  users.push(req.body);
+  res.json({ success: true });
+});
+
+app.get("/api/supports", (req, res) => res.json(supports));
+app.post("/api/supports", (req, res) => {
+  supports.push(req.body);
+  res.json({ success: true });
+});
+
+app.get("/api/home", (req, res) => res.json(home));
+app.post("/api/home", (req, res) => {
+  home.push(req.body);
   res.json({ success: true });
 });
 
